9fad67b517bd2d2cbec65197da7d0cbac34e8293
Bric-wsl <chuxuange@gmail.com>
2024-01-26 15:46:38 +0800

    drawcfg 增加简化选项

--- .script/articulationPoints.py
+++ .script/articulationPoints.py
@@ -4,13 +4,89 @@ from angrutils import *
 import sys
 import random
 
+
+def show_object_info(obj):
+    print(f"Object: {obj}\t Type: {type(obj)}")
+    # 获取对象的所有属性
+    attributes = dir(obj)
+
+    # 打印每个属性和其类型
+    for attr in attributes:
+        if not attr.startswith(('__', 'syscall_name')):  # 过滤掉一些特殊属性
+            attr_value = getattr(obj, attr)
+            attr_type = type(attr_value)
+            print(f"Attribute: {attr}, Type: {attr_type}")
+    print("")
+
+
+def get_function_block(func: angr.knowledge_plugins.functions.function.Function, addr):
+    # 获取特定地址的 BlockNode 对象
+    for block in func.blocks:
+        if block.addr == addr:
+            return block
+    return None
+
+
+def get_function_endaddr(func: angr.knowledge_plugins.functions.function.Function):
+    # 获取函数的终止基本块地址
+    return func.ret_sites[0].addr
+
+def show_graph_nodes(graph):
+    for node in graph.nodes:
+        if node is None:
+            continue
+        # if has attr 'name', check it equal None.
+        if hasattr(node, 'name') and node.name is None:
+            continue
+        if isinstance(node, angr.knowledge_plugins.cfg.cfg_node.CFGNode):
+            print(f"{type(node)}\t{node.name}\t{node.successors}")
+        elif isinstance(node, angr.knowledge_plugins.functions.function.Function):
+            print(f"{type(node)}\t{node.name}")
+        else:
+            print(f"{type(node)}\t{node}")
+
+    print("")
+
+
 def get_articulation_points(target_path):
     print("get articulation_points for [%s]." % (target_path))
     project = angr.Project(target_path, load_options={'auto_load_libs': False})
     cfg = project.analyses.CFGFast()
     cfg.normalize()
-    ap = nx.minimum_node_cut(cfg, flow_func="shortest_augmenting_path")
-    print(ap)
+
+    # show_graph_nodes(cfg.graph)
+
+    # 获取 main 函数的起始地址和终止地址
+    main_function = project.kb.functions.function(name="main")
+    caseb_function = project.kb.functions.function(name="caseb")
+    main_start_address = main_function.addr + 0x46
+    main_end_address = main_function.addr + 0x50
+
+    # 获取 main 函数的 CFGNode
+    start_node = cfg.get_any_node(main_start_address)
+    end_node = cfg.get_any_node(main_end_address)
+
+    # 检测并移除属性值为 'Ijk_FakeRet' 的边
+    edges_to_remove = [(u, v) for u, v, data in cfg.graph.edges(data=True) if data.get('jumpkind') == 'Ijk_FakeRet']
+    cfg.graph.remove_edges_from(edges_to_remove)
+
+    # start_node = get_function_block(main_function, main_start_address)
+    # end_node = get_function_block(main_function, main_end_address)
+    ap = nx.minimum_edge_cut(cfg.graph, start_node, end_node)
+    if ap is None:
+        print("No articulation_points found.")
+        return
+    else:
+        print(f"find {len(ap)} cut nodes.")
+        for node in ap:
+            print(node)
+            print(cfg.graph.get_edge_data(node[0], node[1]))
+
+    # 获取main函数的终止地址
+    main_terminating_address = get_function_endaddr(main_function)
+    print(
+        f"Main function terminating address: {hex(main_terminating_address)} = main+{hex(main_terminating_address - main_function.addr)}")
+
 
 def analyze_binary(binary_path):
     # 创建一个Project对象，加载二进制文件
@@ -24,6 +100,7 @@ def analyze_binary(binary_path):
             if 1 == len(node.predecessors) and node.predecessors[0].name.find("main") != -1:
                 print(f"{func.name}\tpre: {node.predecessors[0].name}")
 
+
 def isUserFunction(angr_function: angr.knowledge_plugins.functions.function.Function) -> bool:
     # Normalize this angr_function first if not normalized.
     if not angr_function.normalized:
@@ -34,33 +111,48 @@ def isUserFunction(angr_function: angr.knowledge_plugins.functions.function.Func
     else:
         return True
 
+
+def getUserFunctionList(cfg: angr.analyses.cfg.cfg_fast.CFGFast) -> list:
+    # Normalize this cfg and func first if not normalized.
+    if not cfg.normalized:
+        cfg.normalize()
+
+    funcList = list()
+    for func in cfg.functions.values():
+        if isUserFunction(func):
+            funcList.append(func.addr)
+    return funcList
+
+
 def funcOnlyMainCall(cfg: angr.analyses.cfg.cfg_fast.CFGFast, func: angr.knowledge_plugins.functions.function.Function) -> bool:
     if not isUserFunction(func):
         return False
-    
+
     node = cfg.get_any_node(func.addr)
 
     if node.predecessors is None:
         return False
-    
+
     if 1 == len(node.predecessors) and node.predecessors[0].name.find("main") != -1:
         return True
-        
+
     return False
-        
+
+
 def getNode(cfg, addr):
     return cfg.get_any_node(addr)
+
 
 def DFSShowFuncNode(cfg, node: angr.knowledge_plugins.cfg.cfg_node.CFGNode):
     if node is None:
         return
-    
+
     print(node.name)
 
     if node.successors is None:
         print(f"{node.name}.successors is None")
         return
-    
+
     for successor in node.successors:
         if isinstance(successor, angr.knowledge_plugins.cfg.cfg_node.CFGNode):
             DFSShowFuncNode(cfg, successor)
@@ -72,23 +164,24 @@ def DFSShowFuncNode(cfg, node: angr.knowledge_plugins.cfg.cfg_node.CFGNode):
                 if isUserFunction(successor):
                     successorNode = getNode(cfg, successor.addr)
                     DFSShowFuncNode(cfg, successorNode)
-                    break                
+                    break
         else:
             # Add to unresolved_block.
             continue
+
 
 def showFuncNodeAddr(binary_path, func_name: str):
     # 创建一个Project对象，加载二进制文件
     project = angr.Project(binary_path, load_options={'auto_load_libs': False})
     cfg = project.analyses.CFGFast()
     cfg.normalize()
-    
+
     # get function by name
     func = cfg.functions.get(func_name, None)
     if func is None:
         print("function [%s] not found." % (func_name))
         return
-    
+
     # get all nodes in this function
     # for addr in func.block_addrs_set:
     #     node = cfg.get_any_node(addr)
@@ -98,7 +191,7 @@ def showFuncNodeAddr(binary_path, func_name: str):
     # get start node
     start_node = cfg.get_any_node(func.addr)
     DFSShowFuncNode(cfg, start_node)
-    
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -112,4 +205,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # showFuncNodeAddr(args.target_path, args.function)
-    analyze_binary(args.target_path)
+    get_articulation_points(args.target_path)
--- .script/drawcfg.py
+++ .script/drawcfg.py
@@ -1,33 +1,114 @@
 import angr
 import sys
 from angrutils import *
+import argparse
 
-def generate_cfg(target_path, output_path):
+
+def isUserFunction(angr_function: angr.knowledge_plugins.functions.function.Function) -> bool:
+    # Normalize this angr_function first if not normalized.
+    if not angr_function.normalized:
+        angr_function.normalize()
+    sys_compiler_funcs = ["_init", "_start", "__do_global_dtors_aux", "frame_dummy", "_fini"]
+    if angr_function.is_plt or angr_function.has_unresolved_jumps or angr_function.is_simprocedure or angr_function.is_default_name or angr_function.is_syscall or angr_function.name in sys_compiler_funcs or 0 == angr_function.size:
+        return False
+    else:
+        return True
+
+
+def getFunc(project, addr: int = None, name: str = None) -> angr.knowledge_plugins.functions.function.Function:
+    # get Angr function object by addr
+    if addr is not None:
+        return project.kb.functions.function(addr=addr)
+    # get Angr function object by name
+    if name is not None:
+        return project.kb.functions.function(name=name)
+    return None
+
+
+def getUserFunctionList(cfg: angr.analyses.cfg.cfg_fast.CFGFast) -> dict:
+    # Normalize this cfg and func first if not normalized.
+    if not cfg.normalized:
+        cfg.normalize()
+
+    funcList = dict()
+    for func in cfg.functions.values():
+        if isUserFunction(func):
+            # add func.addr:func
+            funcList[func.addr] = func
+    return funcList
+
+
+def processGraph(cfg, all_cfg: bool = False, remove_fakeret: bool = False):
+    # Normalize this cfg and func first if not normalized.
+    if not cfg.normalized:
+        cfg.normalize()
+
+    # Remove all edges with jumpkind 'Ijk_FakeRet'
+    if remove_fakeret:
+        edges_to_remove = [(u, v) for u, v, data in cfg.graph.edges(data=True) if data.get('jumpkind') == 'Ijk_FakeRet']
+        cfg.graph.remove_edges_from(edges_to_remove)
+
+    # Remove all edges which are not from main function
+    if not all_cfg:
+        main_function = cfg.kb.functions.function(name="main")
+        target_node = cfg.get_any_node(main_function.addr)
+        subgraphs = list(nx.weakly_connected_components(cfg.graph))
+        target_subgraph = None
+        for subgraph in subgraphs:
+            if target_node in subgraph:
+                target_subgraph = subgraph
+                break
+        # 删除其他节点和边
+        nodes_to_remove = set(cfg.graph.nodes()) - target_subgraph
+        cfg.graph.remove_nodes_from(nodes_to_remove)
+
+    return cfg
+
+
+def generate_cfg(target_path, output_path, use_emulated: bool = False, all_cfg: bool = False, remove_fakeret: bool = False):
     print("Gen cfg for [%s] at main function with output [%s]." % (target_path, output_path))
     project = angr.Project(target_path, load_options={'auto_load_libs': False})
-    #从符号表获取main函数对象
-    main = project.loader.main_object.get_symbol("main")
-    #生成起点从main函数开始的cfg
-    # cfg = project.analyses.CFGEmulated(starts=[main.rebased_addr])
-    # 调用angrutils的方法来画图
-    # plot_cfg(cfg,"cfg",format='png', asminst=True,remove_imports=True)
-    cfg = project.analyses.CFGFast()
-    cfg.normalize()
+
+    if use_emulated:
+        cfg = project.analyses.CFGEmulated()
+    else:
+        cfg = project.analyses.CFGFast()
+
+    cfg = processGraph(cfg, all_cfg, remove_fakeret)
+
     if output_path.endswith('.png') or output_path.endswith('.jpg'):
         output_path = output_path[:-4]  # 去掉结尾的.png或.jpg
-    plot_cfg(cfg, output_path, format='png', asminst=True, remove_imports=True, remove_path_terminator=True)
+    plot_cfg(cfg, output_path, format='png', asminst=True, remove_imports=True,
+             remove_path_terminator=True, remove_simprocedures=True, comments=False)
+
+
+def usage():
+    print("usage: python3 drawcfg.py /path/to/your/target/program -o /path/to/save/output/graph")
+    print("""
+          Generate CFG graph using angr and save it to a PNG file.
+
+          positional arguments:
+            target_path           Path to the target program
+          
+          optional arguments:
+            -h, --help            show this help message and exit
+            -e, --emulated        Use CFGEmulated instead of CGFFast
+            -a, --all-cfg         Draw all CFG, not just main function
+            -r, --remove-fakeret  Remove all edges with jumpkind Ijk_FakeRet
+          """)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print("usage: python3 drawcfg.py /path/to/your/target/program -o /path/to/save/output/graph")
-        sys.exit(-1)
-    import argparse
-    parser = argparse.ArgumentParser(description='Generate CFG graph using angr and save it to a file')
+    parser = argparse.ArgumentParser(
+        description='Generate CFG graph using angr and save it to a PNG file', add_help=True)
     parser.add_argument('target_path', help='Path to the target program')
     parser.add_argument('-o', '--output', help='Path to save the CFG graph', required=True)
+    parser.add_argument('-e', '--emulated', action='store_true',
+                        help='Use CFGEmulated instead of CGFFast', required=False, default=False)
+    parser.add_argument('-a', '--all-cfg', action='store_true',
+                        help='Draw all CFG, not just main function', required=False, default=False)
+    parser.add_argument('-r', '--remove-fakeret', action='store_true',
+                        help='Remove all edges with jumpkind Ijk_FakeRet', required=False, default=False)
     args = parser.parse_args()
 
-    generate_cfg(args.target_path, args.output)
-
-
+    generate_cfg(args.target_path, args.output, args.emulated, args.all_cfg, args.remove_fakeret)
