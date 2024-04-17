2f482f5740c8ac82a20899386fb74c9f07c046eb
hao214 <chuxuange@gmail.com>
2024-04-17 23:09:42 +0800

    以命令行参数测试multi代码覆盖率

--- benchmark/copula/multipath.c
+++ benchmark/copula/multipath.c
@@ -149,24 +149,24 @@ int check_day(int month, int day) {
 int main(int argc, char *argv[]) {
     int year = 0, month = 1, day = 1, total_days = 0;
     // Check if there are enough command line arguments
-    // if (argc >= 2) {
-    //     // Convert command line arguments to integers
-    //     year = atoi(argv[1]);
-    //     if (argc >= 3) {
-    //         month = atoi(argv[2]);
-    //         if (argc >= 4) {
-    //             day = atoi(argv[3]);
-    //         }
-    //     }
-    // } else {
-// If not enough command line arguments, read from stdin
-#ifdef POUT
-        printf("Enter three integers separated by spaces: ");
-#endif
-        char input[100];
-        fgets(input, sizeof(input), stdin);
-        sscanf(input, "%d %d %d", &year, &month, &day);
-    // }
+    if (argc >= 2) {
+        // Convert command line arguments to integers
+        year = atoi(argv[1]);
+        if (argc >= 3) {
+            month = atoi(argv[2]);
+            if (argc >= 4) {
+                day = atoi(argv[3]);
+            }
+        }
+//     } else {
+// // If not enough command line arguments, read from stdin
+// #ifdef POUT
+//         printf("Enter three integers separated by spaces: ");
+// #endif
+//         char input[100];
+//         fgets(input, sizeof(input), stdin);
+//         sscanf(input, "%d %d %d", &year, &month, &day);
+    }
 
     total_days = month_days(month);
     day = check_day(month, day);
@@ -181,9 +181,9 @@ int main(int argc, char *argv[]) {
             total_days++;
         }
     }
-#ifdef POUT
+// #ifdef POUT
     printf("Today is the %d day in this year.\n", total_days);
-#endif
+// #endif
     double *ArrayA = NULL;
     initialize(&ArrayA, total_days);
 
