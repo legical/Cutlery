531fef12431875a9edeabe531f7cd5481951b241
hao214 <chuxuange@gmail.com>
2024-05-02 21:09:09 +0800

    coverage

--- Paper2024/c4/cntCharacters/bench.c
+++ Paper2024/c4/cntCharacters/bench.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+// 检查字符是否为元音字母
+int isVowel(char c) {
+    switch (c) {
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            return 1;
+        default:
+            return 0;
+    }
+}
+
 void countCharacters(const char *str, int *letterCount, int *digitCount, int *spaceCount, int *vowelCount, int *otherCount, int *upperCaseCount, int *lowerCaseCount) {
     *letterCount = 0;
     *digitCount = 0;
@@ -35,12 +54,14 @@ void countCharacters(const char *str, int *letterCount, int *digitCount, int *sp
 void reverseVowels(char *str, int *upperCaseCount, int *lowerCaseCount) {
     int i = -1;
     while (str[++i]) {        
-        if (str[i] == 'a' || str[i] == 'A' || str[i] == 'e' || str[i] == 'E' || str[i] == 'i' || str[i] == 'I' || str[i] == 'o' || str[i] == 'O' || str[i] == 'u' || str[i] == 'U') {
-            if (str[i] >= 'a' && str[i] <= 'z') {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            if (isVowel(str[i])) {
                 str[i] = str[i] - 'a' + 'A';
                 (*upperCaseCount)++;
                 (*lowerCaseCount)--;
-            } else if (str[i] >= 'A' && str[i] <= 'Z') {
+            }            
+        } else if (str[i] >= 'A' && str[i] <= 'Z') {
+            if (isVowel(str[i])) {
                 str[i] = str[i] - 'A' + 'a';
                 (*lowerCaseCount)++;
                 (*upperCaseCount)--;
--- benchmark/copula/multipath.c
+++ benchmark/copula/multipath.c
@@ -125,23 +125,21 @@ int check_day(int month, int day) {
     if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 ||
         month == 10 || month == 12) {
         if (day > 31) {
-            day %= 31;
-            diagram = generate_voronoi(day);
-            
+            day %= 31;            
         }
+        diagram = generate_voronoi(day);
     } else if (month == 4 || month == 6 || month == 9 || month == 11) {
         if (day > 30) {
             day %= 30;
-            diagram = generate_voronoi(day);
-            
         }
+        diagram = generate_voronoi(day);
     } else if (month == 2) {
         if (day > 29) {
             day %= 29;
-            diagram = generate_voronoi(day);
-            
         }
+        diagram = generate_voronoi(day);
     }
+    free_voronoi(diagram);
     return day;
 }
 
